34f38b13e7cf7c08b692217da1f96f9230893cb8
Saisrujan!8 <yayisaisrujan@gmail.com>
2021-03-01 14:04:03 +0530

    Done with beq,bne

--- Addition.asm
+++ Addition.asm
@@ -5,9 +5,12 @@ array:
 .globl main
 main:
     add $t0,$t0,$t1
+    addi $t4,$zero,1
+    addi $t2,$zero,2
     add $t0,$t0,$t1
 exit:
     add $t0,$t0,$t1
-    sub $t0,$t0,$t1
+    sub $t2,$t2,$t4
     addi $t0,$t0,1000
+    bne $t2,$zero,exit
     jr $ra
--- Start.py
+++ Start.py
@@ -3,9 +3,11 @@ import sys
 
 Instructions=[]
 
-Registers={"$s0":0,"$s1":0,"$s2":0,"$s3":0,"$s4":0,"$s5":0,"$s6":0,"$s7":0,"$t0":0,"$t1":10,"$t2":0,"$t3":0,"$t4":0,"$t5":0,"$t6":0,"$t7":0,"$t8":0,"$t9":0,"$zero":0,"$a0":0,"$a1":0,"$a2":0,"$a3":0,"$v0":0,"$v1":0,"$gp":0,"$fp":0,"$sp":0,"$ra":0,"$at":0}
+Registers={"$s0":0,"$s1":0,"$s2":0,"$s3":0,"$s4":0,"$s5":0,"$s6":0,"$s7":0,"$t0":0,"$t1":0,"$t2":0,"$t3":0,"$t4":0,"$t5":0,"$t6":0,"$t7":0,"$t8":0,"$t9":0,"$zero":0,"$a0":0,"$a1":0,"$a2":0,"$a3":0,"$v0":0,"$v1":0,"$gp":0,"$fp":0,"$sp":0,"$ra":0,"$at":0}
 
 Loops={}
+
+jumpRelated=["j","beq","bne"]
 
 Memory=[]
 
@@ -74,24 +76,76 @@ class addi:
             if ok:
                 self.update()
                 return
-        sys.exit()
+            else:
+                sys.exit()
+        else:
+            sys.exit()
     
     def update(self):
         Registers[self.instruction[1]] = Registers[self.instruction[2]] + int(self.instruction[-1])
 
+class beq:
+    instruction=[]
+    indexPosition=0
+    def __init__(self,ins,ind):
+        self.instruction=ins
+        self.indexPosition=ind
+    def check(self):
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,3):
+                ok = ok and checkRegister(self.instruction[i])
+            if self.instruction[-1] not in Loops.keys() :
+                ok=False
+            if ok:
+                return self.update()
+        sys.exit()
+    
+    def update(self):
+        if Registers[self.instruction[1]]==Registers[self.instruction[2]]:
+            return Loops[self.instruction[-1]]
+        return self.indexPosition+1
+
+class bne:
+    instruction=[]
+    indexPosition=0
+    def __init__(self,ins,ind):
+        self.instruction=ins
+        self.indexPosition=ind
+    def check(self):
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,3):
+                ok = ok and checkRegister(self.instruction[i])
+            if self.instruction[-1] not in Loops.keys() :
+                ok=False
+            if ok:
+                return self.update()
+        else:
+            sys.exit()
+    
+    def update(self):
+        if Registers[self.instruction[1]]!=Registers[self.instruction[2]]:
+            return Loops[self.instruction[-1]]
+        return self.indexPosition+1
 
 class control:
     adder=add([])
     subber=sub([])
     addier=addi([])
-    def __init__(self,instruction):
+    beqer=beq([],0)
+    bneer=bne([],0)
+    index=0
+    def __init__(self,instruction,i):
         self.current=instruction
-    
+        self.index=i
     def makeWay(self):
         if len(self.current)==0 :
             sys.exit()
         operation=self.current[0]
-        if operation=="add" :
+        if len(self.current)==1:
+            print("",end="")
+        elif operation=="add" :
             self.adder.__init__(self.current)
             self.adder.check()
         elif operation=="sub":
@@ -100,35 +154,48 @@ class control:
         elif operation=="addi":
             self.addier.__init__(self.current)
             self.addier.check()
+        elif operation=="beq":
+            self.beqer.__init__(self.current,self.index)
+            return self.beqer.check()
+        elif operation=="bne":
+            self.bneer.__init__(self.current,self.index)
+            return self.bneer.check()
         else:
             sys.exit()
 
 with open("Addition.asm") as f:
     Instructions= f.readlines()
-
 for i in range(len(Instructions)):
     Instructions[i]=re.split(" |,|\n",Instructions[i])
     while "" in Instructions[i]:
         Instructions[i].remove('')
-
 InstructionsStartFrom=0
-
 for i in range(len(Instructions)):
     if Instructions[i][0]=="main:":
         InstructionsStartFrom=i+1
         for j in range(InstructionsStartFrom,len(Instructions)):
             if len(Instructions[j])==1:
-                Loops.update({Instructions[j][0]:j})
+                Loops.update({Instructions[j][0][:-1]:j})
         break
-direct=control(Instructions[InstructionsStartFrom])
 
-# print(Loops)
+direct=control([],0)
 
-for i in range(InstructionsStartFrom,InstructionsStartFrom+5):
-    direct.__init__(Instructions[i])
-    direct.makeWay()
+i=InstructionsStartFrom
+while i<len(Instructions):
+    if Instructions[i][0] in jumpRelated:
+        direct.__init__(Instructions[i],i)
+        i=direct.makeWay()
+        print("ef")
+    elif len(Instructions[i])==1:
+        i+=1
+    elif Instructions[i][-1]=="$ra" and Instructions[i][0]=="jr":
+        break
+    else:
+        direct.__init__(Instructions[i],i)
+        direct.makeWay()
+        i+=1
 
 print(Instructions)
 print(Registers)
 print(Memory)
-# print(Loops)
+print(Loops)
