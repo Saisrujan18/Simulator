7f2cb2fdb24de6daea319799ebb8dd4e5cceeded
Manjunath0408 <nath.vasam@gmail.com>
2021-03-02 09:59:51 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,15 +1,263 @@
 import re
 Instructions=[]
-Registers=[]
-Memory=[]
-for i in range(32):
-    Registers.append(0)
+
+Registers={"$s0":0,"$s1":0,"$s2":0,"$s3":0,"$s4":0,"$s5":0,"$s6":0,"$s7":0,"$t0":0,"$t1":0,"$t2":0,"$t3":0,"$t4":0,"$t5":0,"$t6":0,"$t7":0,"$t8":0,"$t9":0,"$zero":0,"$a0":0,"$a1":0,"$a2":0,"$a3":0,"$v0":0,"$v1":0,"$gp":0,"$fp":0,"$sp":0,"$ra":0,"$at":0}
+
+Loops={}
+
+jumpRelated=["j","beq","bne"]
+
+Memory=[0 for i in range(1024)] ## lw , 
+MemoryIndex = 0
+Data = {}
+
+
+def getMemoryIndex():
+    return MemoryIndex
+
+def setMemoryIndex(newVal):
+    MemoryIndex = newVal
+
+def store(data,memIndex):
+    if memIndex >=0 and memIndex < len(Memory):
+        Memory[memIndex] = data
+
+def writeToMemory(Name,data):
+    if Name in Data.keys():
+        print("Error")
+        return
+    i = getMemoryIndex()
+    for it in range(len(data)):
+        store(data[it],i)
+        i+=1
+    setMemoryIndex(i)
+    Data[Name] = i
+
+def parseDataSegment(inst):
+    Name = inst.split(':')[0].strip();
+    Instruction = inst.split(':')[1].strip().split(" ",1)
+    type = Instruction[0]
+    if not type == ".word":
+        return
+    data = Instruction[1].split(',')
+    for i in range(len(data)):
+        data[i] = int(data[i])
+    writeToMemory(Name,data)
+
+
+
+def checkRegister(R):
+    return True if R in Registers.keys() else False
+
+
+class add:
+    instruction=[]
+    def __init__(self,ins):
+        self.instruction=ins
+    
+    def check(self):
+        
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,4):
+                ok = ok and checkRegister(self.instruction[i])
+            if ok:
+                self.update()
+                return
+        sys.exit()
+    
+    def update(self):
+        Registers[self.instruction[1]] = Registers[self.instruction[2]] + Registers[self.instruction[3]]
+
+class sub:
+    instruction=[]
+    def __init__(self,ins):
+        self.instruction=ins
+
+    def check(self):
+        
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,4):
+                ok = ok and checkRegister(self.instruction[i])
+            if ok:
+                self.update()
+                return
+        sys.exit()
+    
+    def update(self):
+        Registers[self.instruction[1]] = Registers[self.instruction[2]] - Registers[self.instruction[3]]
+
+class addi:
+    instruction=[]
+    def __init__(self,ins):
+        self.instruction=ins
+
+    def check(self):
+        
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,4):
+                if i<3 :
+                    ok = ok and checkRegister(self.instruction[i])
+                else:
+                    # ok =ok  and not checkRegister(self.Instruction[-1])
+                    temp=True
+                    if checkRegister(self.instruction[-1])==True:
+                        ok=False
+                        temp=False
+                    else:
+                        ok=ok and temp
+            if ok:
+                self.update()
+                return
+            else:
+                sys.exit()
+        else:
+            sys.exit()
+    
+    def update(self):
+        Registers[self.instruction[1]] = Registers[self.instruction[2]] + int(self.instruction[-1])
+
+class beq:
+    instruction=[]
+    indexPosition=0
+    def __init__(self,ins,ind):
+        self.instruction=ins
+        self.indexPosition=ind
+    def check(self):
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,3):
+                ok = ok and checkRegister(self.instruction[i])
+            if self.instruction[-1] not in Loops.keys() :
+                ok=False
+            if ok:
+                return self.update()
+        sys.exit()
+    
+    def update(self):
+        if Registers[self.instruction[1]]==Registers[self.instruction[2]]:
+            return Loops[self.instruction[-1]]
+        return self.indexPosition+1
+
+class bne:
+    instruction=[]
+    indexPosition=0
+    def __init__(self,ins,ind):
+        self.instruction=ins
+        self.indexPosition=ind
+    def check(self):
+        if len(self.instruction)== 4: 
+            ok = True
+            for i in range(1,3):
+                ok = ok and checkRegister(self.instruction[i])
+            if self.instruction[-1] not in Loops.keys() :
+                ok=False
+            if ok:
+                return self.update()
+        else:
+            sys.exit()
+    
+    def update(self):
+        if Registers[self.instruction[1]]!=Registers[self.instruction[2]]:
+            return Loops[self.instruction[-1]]
+        return self.indexPosition+1
+
+class j:
+    instruction=[]
+    def __init__(self,ins):
+        self.instruction=ins
+    def check(self):
+        if len(self.instruction)==2: 
+            ok = True
+            if self.instruction[-1] not in Loops.keys() :
+                ok=False
+            if ok:
+                return self.update()
+        sys.exit()
+    
+    def update(self):
+        return Loops[self.instruction[-1]]
+
+class control:
+    adder=add([])
+    subber=sub([])
+    addier=addi([])
+    beqer=beq([],0)
+    bneer=bne([],0)
+    jer=j([])
+    index=0
+    def __init__(self,instruction,i):
+        self.current=instruction
+        self.index=i
+    def makeWay(self):
+        if len(self.current)==0 :
+            sys.exit()
+        operation=self.current[0]
+        if len(self.current)==1:
+            print("",end="")
+        elif operation=="add" :
+            self.adder.__init__(self.current)
+            self.adder.check()
+        elif operation=="sub":
+            self.subber.__init__(self.current)
+            self.subber.check()
+        elif operation=="addi":
+            self.addier.__init__(self.current)
+            self.addier.check()
+        elif operation=="beq":
+            self.beqer.__init__(self.current,self.index)
+            return self.beqer.check()
+        elif operation=="bne":
+            self.bneer.__init__(self.current,self.index)
+            return self.bneer.check()
+        elif operation=='j':
+            self.jer.__init__(self.current)
+            return self.jer.check()
+        else:
+            sys.exit()
+
 with open("Addition.asm") as f:
     Instructions= f.readlines()
 for i in range(len(Instructions)):
     Instructions[i]=re.split(" |,|\n",Instructions[i])
     while "" in Instructions[i]:
         Instructions[i].remove('')
+<<<<<<< Updated upstream
 print(Instructions)
 print(Registers)
 print(Memory)
+=======
+InstructionsStartFrom=0
+for i in range(len(Instructions)):
+    if Instructions[i][0]=="main:":
+        InstructionsStartFrom=i+1
+        for j in range(InstructionsStartFrom,len(Instructions)):
+            if len(Instructions[j])==1:
+                Loops.update({Instructions[j][0][:-1]:j})
+        break
+
+direct=control([],0)
+
+i=InstructionsStartFrom
+while i<len(Instructions):
+    if Instructions[i][0] in jumpRelated:
+        direct.__init__(Instructions[i],i)
+        i=direct.makeWay()
+    elif len(Instructions[i])==1:
+        i+=1
+    elif Instructions[i][-1]=="$ra" and Instructions[i][0]=="jr":
+        break
+    else:
+        direct.__init__(Instructions[i],i)
+        direct.makeWay()
+        i+=1
+
+# writeToMemory("loop",[1])
+parseDataSegment('loop : .word 1,2,3')
+print(Data)
+# print(Registers)
+# print(Memory)
+# print(Loops)
+>>>>>>> Stashed changes
