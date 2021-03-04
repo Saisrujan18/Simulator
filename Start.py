db6ce50a73e2e6f2d5e1b4bfe0aefb1ab4787271
Saisrujan!8 <yayisaisrujan@gmail.com>
2021-03-04 12:21:50 +0530

    Tabs
    removing \t

--- Start.py
+++ Start.py
@@ -312,6 +312,10 @@ for i in range(len(Instructions)):
 while [] in Instructions:
     Instructions.remove([])
 
+for i in range(len(Instructions)):
+    for j in range(len(Instructions[i])):
+        while "\t" in Instructions[i][j]:
+            Instructions[i][j]=Instructions[i][j].replace("\t","")
 
 if [".text"]  in Instructions:
     temp1=Instructions.index([".text"])
@@ -367,6 +371,8 @@ for i in range(len(Instructions)):
 
 for i in range(1,whereistext):
     AddToMemory(Instructions[i])
+
+
     
 # <<<<<<< MANIPULATING INSTRUCTIONS ENDS HERE
 InstructionsStartFrom=len(Instructions)
@@ -399,7 +405,7 @@ while i<len(Instructions):
 
 
 print(Instructions)
-# print(Registers)
+print(Registers)
 print(Memory)
-# print(Loops)
-# print(Data)
+print(Loops)
+print(Data)
