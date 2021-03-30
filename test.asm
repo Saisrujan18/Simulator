67ff1db44384f515f6b036e286963dc725783a41
Manjunath0408 <nath.vasam@gmail.com>
2021-03-30 14:54:22 +0530

    Update test.asm

--- test.asm
+++ test.asm
@@ -4,16 +4,6 @@ array:
 .text 
 .globl main  
 main:   
-    add $t0,$t0,$t1
-    addi $t4,$zero,1
-    addi $t2,$zero,2
-    add $t0,$t0,$t1
-exit:
-    add $t0,$t0,$t1
-    sub $t2,$t2,$t4
-    addi $t0,$t0,1000
-    bne $t2,$zero,exit
-    j theend
-    addi $t0,$t0,1000
-theend :
-    jr $ra
+    add $t0,$t1,$t2
+    add $t3,$t0,$t2
+    add $t5,$t3,$t2
