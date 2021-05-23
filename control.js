a1585022a7087b6ebdcb2ad3609cb16256f0d356
Saisrujan18 <yayisaisrujan@gmail.com>
2021-05-23 15:53:39 +0530

    GT

---
+++ control.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+fs.writeFile("InputFile","hello mf");
+// var fs=
+// function save()
+// {
+    // const fileInput = document.getElementById('input');
+    // fileInput.onchange = () => 
+    // {
+    //     const selectedFile = fileInput.files[0];
+    //     console.log(selectedFile);
+    // }
+    // let data = "Learning how to write in a file.";
+    // fs.writeFile('InputFile', data, (err) => {if(err) throw err;});
+// }
